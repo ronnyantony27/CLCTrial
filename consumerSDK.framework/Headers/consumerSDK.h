692c24bb09155cf739b1419f8d470c4f903a4c50
Ronny Antony <ronny.antony@zcocorporation.com>
2021-07-16 15:13:12 +0530

    Initial Framework commit.

---
+++ consumerSDK.framework/Headers/consumerSDK.h
@@ -0,0 +1,17 @@
+//
+//  consumerSDK.h
+//  consumerSDK
+//
+//  Created by ZCO Engineering Dept. on 17/02/21.
+//  Copyright © 2021 cueLess. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for consumerSDK.
+FOUNDATION_EXPORT double consumerSDKVersionNumber;
+
+//! Project version string for consumerSDK.
+FOUNDATION_EXPORT const unsigned char consumerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <consumerSDK/PublicHeader.h>
